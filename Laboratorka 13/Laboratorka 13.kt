2f24933fde789a510237dd0cbdacfb8c9b82fffc
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-05-04 20:58:31 +0300

    Laboratorka 13. Task 5

--- Laboratorka 13/Laboratorka 13.kt
+++ Laboratorka 13/Laboratorka 13.kt
@@ -8,3 +8,15 @@ fun main(args: Array<String>)
     val s = readLine()
     println("Hello, $s")
 }
+
+fun main(args: Array<String>)
+{
+    println("Какой язык у вас любимый?")
+    val language = readLine()
+    when (language)
+    {
+        "Kotlin" -> println("Ясно, подлиза")
+        "Prolog" -> println("Ясно, подлиза")
+        else -> println("Ничоси :О")
+    }
+}
