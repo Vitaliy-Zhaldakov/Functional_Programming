8abe56397881866c7b73544ec8dfcd1385c65f09
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-05-21 23:31:09 +0300

    Laboratorka 14. Task 4

--- Laboratorka 14/Laboratorka 14.kt
+++ Laboratorka 14/Laboratorka 14.kt
@@ -20,3 +20,17 @@ tailrec fun maxDigitDown(digit:Int, curMax:Int):Int = if (digit != 0) if (digit%
 fun mulDigitsUp(digit:Int):Int = if (digit != 0) digit%10 * mulDigitsUp(digit/10) else 1
 //Произведение цифр числа хвостовой рекурсией
 tailrec fun mulDigitsDown(digit:Int, mul:Int):Int = if (digit != 0) mulDigitsDown(digit/10, digit%10*mul) else mul
+
+//Функция обхода числа, выполняющая заданные операции над числом
+fun numberTraversal(digit: Int, init: Int = 0, operation:(Int,Int) -> Int) = operation(digit,init)
+
+fun main()
+{
+    val scanner = Scanner(System.`in`)
+        print("Введите число: ")
+        val digit = scanner.nextInt()
+        println(numberTraversal(digit,0) { digit, init -> sumDigitsDown(digit, init) })
+        println(numberTraversal(digit,1) { digit, init -> mulDigitsDown(digit, init) })
+        println(numberTraversal(digit,digit%10) { digit, init -> minDigitDown(digit, init) })
+        println(numberTraversal(digit,digit%10) { digit, init -> maxDigitDown(digit, init) })
+}
