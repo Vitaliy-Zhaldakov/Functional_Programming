ab744dbce5ba5a54681a377fca30558b27cf632b
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-05-21 21:30:35 +0300

    Laboratorka 14. Task 2

--- Laboratorka 14/Laboratorka 14.kt
+++ Laboratorka 14/Laboratorka 14.kt
@@ -2,3 +2,5 @@ import java.util.Scanner
 
 //Сумма цифр числа рекурсией вверх
 fun sumDigitsUp(digit:Int):Int =  if (digit != 0) digit%10 + sumDigitsUp(digit/10) else 0
+//Сумма цифр числа рекурсией вниз
+tailrec fun sumDigitsDown(digit:Int, sum:Int):Int =  if (digit != 0) sumDigitsDown(digit/10, sum+digit%10) else sum
